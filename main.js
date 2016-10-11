07bddfc04e9965315b2e6e663fbc7d6efbfb84bc
Shawn M <forgeforza@outlook.com>
2016-10-11 11:24:19 -0500

    intial Product page commit

---
+++ main.js
@@ -0,0 +1,61 @@
+var productarr  = [Juice// javascript all up in here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var arr= []
+var myObject ={}
+var song = {
+	title: "Call me Maybe"
+	artist: "Carly Jae Pop Star"
+	album: "Cool Songs"
+	awards: ["none", "blah"], // <--value is an array
+	type: { //<- value is another object
+		style: "pop"
+		listenability: "none"
+	producers : { 
+		name: "Freddie Jones"
+		name2: "Sally Smith"}
+		}
+	}	
+listAwards: function(){      <// object listAwards now has a value of function
+	console.log("I won no awards");
+}
+
+}
+
+console.log (song.type.producers.name2)
+
+song.awards [1]
+song.type.releaseYear= "2012"
+
+
+song["album"]
+
+// grab an array of properties from an object //
+//console.log (Object.keys(song));
+
+The lazy dig likes the weird fix. 
+
+var xyz = "hidden Objects";
+
+
+object.push  // pushing or 'loading' objects upstream
+
+for (var i=0); 
+
+
+//bracket notation  in quotes = use actual key name 
+// no quotes === use variable var
+
+
